acd1720ab849988b969d8a59991111e35a8d88d3
JEFFNAILY CHARLES <jeffnaily_charles@estudante.sesisenai.org.br>
2026-06-23 16:01:24 -0300

    texto

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+void despedida() {
+	cout << "Ate breve! " << endl;
+}
+
+void saudacao() {
+	cout << "Ola" << endl;
+
+}
+
 int main() {
-	
+	despedida ();
+	saudacao ();
+		
 	cout << "Projeto em Grupo" << endl;
 	
 	return 0;
+	
 }
